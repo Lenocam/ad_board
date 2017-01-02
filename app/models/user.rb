766c403e7f4a64f7bc6c1530935f6ed48581071b
Reed MacConnell <Learn22Ruby@gmail.com>
2017-01-02 13:42:42 -0500

    added api/v1 controllers and sign in and out routes. added auth_token to user model created api sign on and out

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,8 +11,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-
-
   has_many :galleries, dependent: :destroy
+
+  def generate_auth_token
+    token = SecureRandom.hex
+    self.update_columns(auth_token: token)
+    token
+  end
+
+  def invalidate_auth_token
+    self.update_columns(auth_token: nil)
+  end
 end
