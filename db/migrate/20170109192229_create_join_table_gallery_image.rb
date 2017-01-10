3cd8266f9df9701b1c170c42a9eaa5b6aac61518
Reed MacConnell <Learn22Ruby@gmail.com>
2017-01-10 16:11:03 -0500

    deleted un-needed migrations, keeping incase that was stupid of me

--- db/migrate/20170109192229_create_join_table_gallery_image.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGalleryImage < ActiveRecord::Migration[5.0]
-  def change
-    create_join_table :galleries, :images do |t|
-      t.index [:gallery_id, :image_id]
-      t.index [:image_id, :gallery_id]
-    end
-  end
-end
