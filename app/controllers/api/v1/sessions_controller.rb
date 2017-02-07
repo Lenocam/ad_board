dd84ea179f2e0fdd52238c6053c7f18a3635d87f
Reed MacConnell <Learn22Ruby@gmail.com>
2017-02-07 10:40:10 -0500

    added ajax, modals and cleaner creation experience for galleries and images

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -5,6 +5,7 @@ module Api
 
       def create
         resource = User.find_for_database_authentication(:email => params[:user_login][:email])
+        
         return invalid_login_attempt unless resource
 
         if resource.valid_password?(params[:user_login][:password])
@@ -23,7 +24,7 @@ module Api
 
       private
         def invalid_login_attempt
-          render json: { errors: [ { detail:"Error with you login or password"}]}, status: 401
+          render json: { errors: [ { detail:"Error with your login or password"}]}, status: 401
         end
     end
   end
